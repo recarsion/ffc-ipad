2d1c77e11231681b7ac551f3e0cdbeedcb365a18
Carlos Guinto <carlosvguinto@gmail.com>
2023-05-12 12:21:59 +0800

    add supplier button and modal

--- src/app/masterlist/masterlist.component.ts
+++ src/app/masterlist/masterlist.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { ModalService } from '../services/modal.service';
 
 @Component({
   selector: 'app-masterlist',
   templateUrl: './masterlist.component.html',
-  styleUrls: ['./masterlist.component.css']
+  styleUrls: ['./masterlist.component.css'],
 })
 export class MasterlistComponent {
+  modalTitle = '';
+  constructor(public modal: ModalService) {}
 
+  addSupplier() {
+    this.modalTitle = 'Add Supplier';
+    this.modal.toggleModal('supplier');
+    console.log('addSupplier called');
+  }
 }
